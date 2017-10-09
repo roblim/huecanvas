1553d84a452eebb5c42e4d06aeb8c4517a872f97
snakerabbit <alison.s.cheng@gmail.com>
2017-10-09 15:10:58 -0700

    import necessary react-native features

--- components/Rooms Index/rooms_index.js
+++ components/Rooms Index/rooms_index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import RoomsIndexItem from './rooms_index_item';
 
 class RoomsIndex extends Component{
--- components/Rooms Index/rooms_index_item.jsx
+++ components/Rooms Index/rooms_index_item.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 
 class RoomsIndexItem extends Component{
