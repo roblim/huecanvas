4be629e68c79321177ca964f71fab0940a8b3868
snakerabbit <alison.s.cheng@gmail.com>
2017-10-09 14:59:54 -0700

    added actions for rooms index

--- actions/rooms_index_actions.js
+++ actions/rooms_index_actions.js
@@ -1,5 +1,8 @@
 import {api} from './util/rooms_index_api_util';
 export const RECEIVE_LIGHTS = "RECEIVE_LIGHTS";
+export const RECEIVE_ROOMS = "RECEIVE_ROOMS";
+export const RECEIVE_ROOM = "RECEIVE_ROOM";
+
 
 const receiveLights = lights =>{
   return({
@@ -8,6 +11,30 @@ const receiveLights = lights =>{
   });
 };
 
+const receiveRooms = rooms =>{
+  return({
+    type: RECEIVE_ROOMS,
+    rooms
+  });
+};
+
+const receiveRoom = room =>{
+  return({
+    type: RECEIVE_ROOM,
+    room
+  });
+};
+
+//to set rooms data: localStorage.setItem('rooms', rooms);
+
 export const fetchLights = () => dispatch =>(
   api.lights().then(lights => dispatch(receiveLights(lights)))
+);
+
+export const fetchRooms = () => dispatch =>(
+  localStorage.getItem().then(rooms => dispatch(receiveRooms(rooms)))
+);
+
+export const fetchRoom = roomId => dispatch =>(
+  localStorage.getItem(roomId).then(room => dispatch(receiveRoom(room)))
 );
---
+++ reducers/rooms_index_reducer.js
@@ -0,0 +1,6 @@
+import { RECEIVE_LIGHTS} from '../actions/rooms_index_actions';
+import merge from 'lodash/merge';
+
+const RoomsIndexReducer = (state,action) =>{
+  
+};
