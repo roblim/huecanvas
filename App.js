5646a42f8c5ea237c8d4b554a034720747e9196c
Cory Kane <ckane30691@gmail.com>
2017-10-09 18:09:57 -0700

    Begin setting up redux elements

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import configureStore from './store/store';
+// import Root from './components/root.jsx';
 
 export default class App extends React.Component {
   render() {
+    let store = configureStore()
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Root store={store} />
     );
   }
 }
