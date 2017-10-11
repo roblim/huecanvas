c19f300678bfa74aa87d0054933fbb5a42719579
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-10-10 17:27:11 -0700

    fix discover call

--- components/discover/discover.js
+++ components/discover/discover.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Button, StyleSheet, View } from "react-native";
 
 class Discover extends React.Component {
   constructor(props) {
     super(props);
-
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     this.props.fetchBridges();
   }
 
+  handleClick() {
+    let bridges = this.props.fetchBridges();
+    console.log(bridges);
+  }
+
   render() {
     return (
+      <View style={styles.box}>
       <Text>discover!</Text>
+      <Button onPress={this.handleClick}
+              title="get bridges"
+          />
+        </ View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "grey"
+  }
+})
 export default Discover;
