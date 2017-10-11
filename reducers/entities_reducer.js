bcbb7e9c569bc552133dfbfad6d83c4f1813afaf
Rob Lim <robert.m.lim@gmail.com>
2017-10-10 22:39:05 -0700

    Update entities_reducer.js

--- reducers/entities_reducer.js
+++ reducers/entities_reducer.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import { combineReducers } from "redux";
-import entities from "./scenes_reducer";
-
-export default combineReducers({
- entities;
-});
-
-export default RootReducer;
-=======
 import { combineReducers } from 'redux';
 import rooms from './rooms_reducer'
 
 export default combineReducers({
 	rooms
 });
->>>>>>> origin/room_form_component
