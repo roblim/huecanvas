961ec6342af80b8a123df04c873107338fc04b76
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-10-11 22:53:12 -0700

    fetchLights

--- reducers/entities_reducer.js
+++ reducers/entities_reducer.js
@@ -3,7 +3,6 @@ import rooms from './rooms_reducer'
 import scenes from "./scenes_reducer";
 import lights from "./discover_reducer";
 
-
 export default combineReducers({
   rooms,
   scenes,
--- reducers/root_reducer.js
+++ reducers/root_reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import entities from './entities_reducer';
-// import errors from './errors_reducer';
 
 const RootReducer = combineReducers({
   entities,
