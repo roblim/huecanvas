12a60a6a00e2dc8e6b4ae0f97280ef6a5362cc92
Cory Kane <ckane30691@gmail.com>
2017-10-16 02:03:14 -0700

    comment out unused route

--- components/navigator.js
+++ components/navigator.js
@@ -5,11 +5,13 @@ import RoomFormContainer from './rooms/room_form_container';
 import RoomsIndexContainer from './rooms/rooms_index_container';
 import Root from './root';
 import NavSlidersContainer from './nav/nav_sliders_container'
+// import RoomTemp from "./rooms/room_temp";
 
 const TabNavigator = StackNavigator({
 	home: {screen: Root},
 	roomsNew: {screen: RoomFormContainer},
-	roomsIndex: {screen: RoomsIndexContainer}
+	roomsIndex: {screen: RoomsIndexContainer},
+	// roomTemp: {screen: RoomTemp}
 }, {
   navigationOptions: {
     headerTintColor: 'white',
